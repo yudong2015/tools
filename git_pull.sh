9b7917291cc997d2c995b2f5ffce968c594649bf
jackyu <jackyu@yunify.com>
2019-11-07 12:04:23 +0800

    update git operate

--- git_pull.sh
+++ git_pull.sh
@@ -8,12 +8,16 @@ CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 
 echo "CURRENT_BRANCH: ${CURRENT_BRANCH}"
 
-if [[ ${TARGET_BANCH} == ""]]; then
+if [[ ${TARGET_BANCH} == "" ]]; then
     TARGET_BANCH=${CURRENT_BRANCH}
+    echo "git pull ${REGISTRY} ${TARGET_BANCH}"
     git pull ${REGISTRY} ${TARGET_BANCH}
 else
+    echo "git checkout ${TARGET_BANCH}"
     git checkout ${TARGET_BANCH}
+    echo "git pull ${REGISTRY} ${TARGET_BANCH}"
     git pull ${REGISTRY} ${TARGET_BANCH}
+    echo "git checkout ${CURRENT_BRANCH}"
     git checkout ${CURRENT_BRANCH}
 fi
 
--- git_push.sh
+++ git_push.sh
@@ -7,10 +7,14 @@ TARGET_BANCH=$2
 CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 
 if [[ ${TARGET_BANCH} == "" ]]; then
+    echo "git push -u ${REGISTRY} ${CURRENT_BRANCH}"
     git push -u ${REGISTRY} ${CURRENT_BRANCH}
 else
+    echo "git checkout ${TARGET_BANCH}"
     git checkout ${TARGET_BANCH}
+    echo "git push -u ${REGISTRY} ${TARGET_BANCH}"
     git push -u ${REGISTRY} ${TARGET_BANCH}
+    echo "git checkout ${CURRENT_BRANCH}"
     git checkout ${CURRENT_BRANCH}
 fi    
 
