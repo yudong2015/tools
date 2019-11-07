84f46eabcbbd68d9977b3b3781ec8f86daa3f3cf
jackyu <jackyu@yunify.com>
2019-11-07 17:20:40 +0800

    idea keymap

--- git_pull.sh
+++ git_pull.sh
@@ -6,7 +6,6 @@ REGISTRY=$1
 TARGET_BANCH=$2
 CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 
-echo "CURRENT_BRANCH: ${CURRENT_BRANCH}"
 
 if [[ ${TARGET_BANCH} == "" ]]; then
     TARGET_BANCH=${CURRENT_BRANCH}
